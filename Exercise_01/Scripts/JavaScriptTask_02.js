77f8bc2aeb625d36aa6587b4bcaaf8db79005f43
Jakob Stjernholm <jakob.stjerna@gmail.com>
2015-12-21 12:24:30 +0100

    Task 02 in Labb 1

---
+++ Exercise_01/Scripts/JavaScriptTask_02.js
@@ -0,0 +1,15 @@
+﻿var numberOne = document.getElementById('numberOne').value;
+var numberTwo = document.getElementById('numberTwo').value;
+
+document.getElementById('myButton').addEventListener('click', calculate, false);
+
+function calculate() {
+    if (numberOne > numberTwo) {
+        document.getElementById('answer').innerHTML = numberOne;
+    }
+    else if (numberOne< numberTwo) {
+        document.getElementById('answer').innerHTML = numberTwo;
+    } else {
+        document.getElementById('answer').innerHTML = "The numbers are the same";
+    }
+}
