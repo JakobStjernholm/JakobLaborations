78ad72a2614b40ba4f36234659f5940f94f072c5
Jakob Stjernholm <jakob.stjerna@gmail.com>
2015-12-21 12:30:38 +0100

    Task 02 done

--- Exercise_01/Scripts/JavaScriptTask_02.js
+++ Exercise_01/Scripts/JavaScriptTask_02.js
@@ -1,13 +1,14 @@
-﻿var numberOne = document.getElementById('numberOne').value;
-var numberTwo = document.getElementById('numberTwo').value;
+﻿
 
 document.getElementById('myButton').addEventListener('click', calculate, false);
 
 function calculate() {
+    var numberOne = document.getElementById('numberOne').value;
+    var numberTwo = document.getElementById('numberTwo').value;
     if (numberOne > numberTwo) {
         document.getElementById('answer').innerHTML = numberOne;
     }
-    else if (numberOne< numberTwo) {
+    else if (numberOne < numberTwo) {
         document.getElementById('answer').innerHTML = numberTwo;
     } else {
         document.getElementById('answer').innerHTML = "The numbers are the same";
