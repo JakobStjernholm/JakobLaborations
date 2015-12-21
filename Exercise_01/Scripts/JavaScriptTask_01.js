7293037d05478164a0d4866a01654f0f8d774a8b
Jakob Stjernholm <jakob.stjerna@gmail.com>
2015-12-21 11:26:48 +0100

    Done Task 01

--- Exercise_01/Scripts/JavaScriptTask_01.js
+++ Exercise_01/Scripts/JavaScriptTask_01.js
@@ -1,1 +1,8 @@
-﻿var swedishKronor = getElementById('currencyInput').innerHTML;
+﻿
+document.getElementById('convertKronor').addEventListener('click', convertTo, true);
+function convertTo() {
+    
+    var swedishKronor = document.getElementById('currencyInput').value;
+    document.getElementById('euroParagraph').innerHTML = swedishKronor * 0.092826;
+    document.getElementById('dollarParagraph').innerHTML = swedishKronor * 0.11694;
+}
