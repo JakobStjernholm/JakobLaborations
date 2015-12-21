61b1e6a275b0837229d53365bf07d61847bbddb1
Jakob Stjernholm <jakob.stjerna@gmail.com>
2015-12-21 13:53:09 +0100

    Halfway through Task 04

---
+++ Exercise_01/Scripts/JavaScriptTask_04.js
@@ -0,0 +1,14 @@
+﻿
+document.getElementById('myBtn').addEventListener('click',numberCalculation,false);
+
+
+function numberCalculation() {
+    var inputNumber = document.getElementById('inputNumber').value;
+    if (inputNumber !== 0) {
+        var element = document.createElement('p');
+        element.textContent = inputNumber;
+        var diven = document.getElementById('answer-div');
+        diven.appendChild(element);
+    }
+    
+}
