29023d27c46a556521c7ffbbef18de5a201cf82f
Jakob Stjernholm <jakob.stjerna@gmail.com>
2015-12-21 13:57:04 +0100

    Task04 done

--- Exercise_01/Scripts/JavaScriptTask_04.js
+++ Exercise_01/Scripts/JavaScriptTask_04.js
@@ -4,11 +4,16 @@ document.getElementById('myBtn').addEventListener('click',numberCalculation,fals
 
 function numberCalculation() {
     var inputNumber = document.getElementById('inputNumber').value;
-    if (inputNumber !== 0) {
+    if (inputNumber != 0) {
         var element = document.createElement('p');
         element.textContent = inputNumber;
         var diven = document.getElementById('answer-div');
         diven.appendChild(element);
+    } else {
+        var elementH = document.createElement('h1');
+        elementH.textContent = "GAME OVER ! Du har nu skrivit in en nolla(0), sidan är nu begagnad och använd, hitta ett nytt spel...";
+        var divenH = document.getElementById('answer-div');
+        divenH.appendChild(elementH);
     }
     
 }
