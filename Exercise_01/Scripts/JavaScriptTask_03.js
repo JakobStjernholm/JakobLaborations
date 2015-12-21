f65e8a5834bcf75c20ea5dc3424e71fd74e3bedb
Jakob Stjernholm <jakob.stjerna@gmail.com>
2015-12-21 12:52:43 +0100

    Task03 done

--- Exercise_01/Scripts/JavaScriptTask_03.js
+++ Exercise_01/Scripts/JavaScriptTask_03.js
@@ -1,1 +1,14 @@
 ﻿
+document.getElementById('myButton').addEventListener('click', calculate, false);
+
+function calculate() {
+    var firstNumber = document.getElementById('firstNumber').value;
+    var secondNumber = document.getElementById('secondNumber').value;
+    if (((secondNumber * 2) - firstNumber) > 0) {
+        document.getElementById('answer').innerHTML = "Talet " + firstNumber + " är inte dubbelt så stort som " + secondNumber;
+    }
+    else if (((secondNumber * 2) - firstNumber) <= 0) {
+        document.getElementById('answer').innerHTML = "Talet " + firstNumber + " är  dubbelt så stort som " + secondNumber + " eller mer.";
+    }
+    
+}
