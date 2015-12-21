443382bdd52c3fcc8cc86ed3e5105ece55a7a00a
Jakob Stjernholm <jakob.stjerna@gmail.com>
2015-12-21 14:24:03 +0100

    Task 05 is ongoing

---
+++ Exercise_01/Scripts/JavaScriptTask_05.js
@@ -0,0 +1,15 @@
+﻿document.getElementById('btn').addEventListener('click', myFunction, false);
+
+
+function myFunction() {
+    var grader = document.getElementById('dropdown').value;
+    var strUser = grader.options[grader.selectedIndex].value;
+    var number = document.getElementById('grader').text;
+    if (strUser === 1) {
+        document.getElementById('ett').innerHTML = (number - 32) * (5 / 9);
+        alert((number - 32) * (5 / 9));
+    } else {
+        document.getElementById('ett').innerHTML = number * (9 / 5) + 32;
+        alert(number * (9 / 5) + 32);
+    }
+}
