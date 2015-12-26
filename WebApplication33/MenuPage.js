47035c07f30c921d12371368d6b77200659f274b
Jakob Stjernholm <jakob.stjerna@gmail.com>
2015-12-26 13:41:35 +0100

    Added todaysoffer method, using switch case.

--- WebApplication33/MenuPage.js
+++ WebApplication33/MenuPage.js
@@ -1,7 +1,7 @@
 ﻿//happy hour logic:
 function happyHour() {
     var happyHourTime = 18;
-    var datetimeNow = new Date(2015, 10, 10, 17);
+    var datetimeNow = new Date(2015, 10, 10, 18);
 
     if (datetimeNow.getHours() === happyHourTime) {
         var prices = document.getElementsByClassName('price');
@@ -10,12 +10,11 @@ function happyHour() {
             prices[i].innerHTML = 16.90;
         }
     }
-
 }
 
 //Method calls:
 happyHour();
-
+todaysOffer();
 //Adjust price function: (add .toFixed(2) for 2 decimals)
 
 /*Add pictures function to span:*/
@@ -23,6 +22,51 @@ happyHour();
 /*Add todays offer function. 
 Make price background red. 
 Reduce price by another 20%:*/
+
+function todaysOffer() {
+    var date = new Date(2015, 10, 26);
+    var dayOfDate = date.getDay();
+
+    switch (true) {
+        case dayOfDate == 0:
+            document.getElementById('sunday').style["backgroundColor"] = "red";
+        var price = document.getElementsById('sunday').innerHTML;
+        document.getElementsById('sunday').innerHTML = (price * 0.8);
+        break;
+        case dayOfDate == 1:
+            document.getElementById('monday').style["backgroundColor"] = "red";
+            price = document.getElementById('monday').innerHTML;
+            document.getElementById('monday').innerHTML = (price * 0.8);
+            break;
+        case dayOfDate == 2:
+            document.getElementById('tuesday').style["backgroundColor"] = "red";
+            price = document.getElementById('tuesday').innerHTML;
+            document.getElementById('tuesday').innerHTML = (price * 0.8);
+            break;
+        case dayOfDate == 3:
+            document.getElementById('wednesday').style["backgroundColor"] = "red";
+            price = document.getElementById('wednesday').innerHTML;
+            document.getElementById('wednesday').innerHTML = (price * 0.8);
+            break;
+        case dayOfDate == 4:
+            document.getElementById('thursday').style["backgroundColor"] = "red";
+            price = document.getElementById('thursday').innerHTML;
+            document.getElementById('thursday').innerHTML = (price * 0.8);
+            break;
+        case dayOfDate == 5:
+            document.getElementById('friday').style["backgroundColor"] = "red";
+            price = document.getElementById('friday').innerHTML;
+            document.getElementById('friday').innerHTML = (price * 0.8);
+            break;
+        case dayOfDate == 6:
+            document.getElementById('saturday').style["backgroundColor"] = "red";
+            price = document.getElementById('satruday').innerHTML;
+            document.getElementById('saturday').innerHTML = (price * 0.8);
+            break;
+        default:
+            break;
+    }
+}
 
 /*Stretch exercises: (increasing tuffness for each number!)
 
