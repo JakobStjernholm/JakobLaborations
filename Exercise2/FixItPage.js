b73f786856847af54a8f57769617662978e91612
Jakob Stjernholm <jakob.stjerna@gmail.com>
2016-01-03 11:40:22 +0100

    Playing around with window properties.

--- Exercise2/FixItPage.js
+++ Exercise2/FixItPage.js
@@ -1,38 +1,63 @@
-﻿/*
+﻿
 
-For each of the following aplicable exercise below you should present a box on the page 
-with an alert that shows the result when the box is clicked!
+//For each of the following aplicable exercise below you should present a box on the page 
+//with an alert that shows the result when the box is clicked!
 
-###Window object at page 124:###
-1. Link the script with FixIt.hmtl
-2. Add som tags that doesn't change the visual part of the paragraph. -------Behövs inte
-3.Play with the properties at page 124 in the book. 
-  Use properties creatively to display things at the html page
-4.Add a stylesheet that displays the changed words in fat font and in red.
-5. PLay with the Methods page 126
-### DOM page 126:###
-6. Play with the DOM-properties
+//###Window object at page 124:###
+//1. Link the script with FixIt.hmtl
+//2. Add som tags that doesn't change the visual part of the paragraph. -------Behövs inte
+//3.Play with the properties at page 124 in the book. 
+//  Use properties creatively to display things at the html page
 
-### String objects page 128, 129: ###
 
-7. Save the the text from the makeMeAnArray-paragraph into an array.
-8. Use all the string methods and propertys allong with the array
+var createdDiv = document.createElement('div');
+createdDiv.id = 'clickableDiv';
 
-### String objects page 132: ###
-9. check if the 4th element in the array is a number
+var body = document.getElementsByTagName('body')[0].appendChild(createdDiv);
 
-### Math page 134: ###
-10. Round one of the numbers in the paragraph up/down
-11. replace the 3rd word with PI then roud it to the nearest integer
+var innerDiv = document.createElement('div');
+innerDiv.id = 'innerDiv';
+innerDiv.innerHTML = 'Click me to see window properties!';
+innerDiv.style.backgroundColor = 'blue';
+createdDiv.appendChild(innerDiv);
 
-### Date object### page 137
-12. Calculate how many days it's until your birthday and present it.
-13. Calculate how many minutes old you are and present it.
+document.getElementById('innerDiv').addEventListener('click', myMethod, false);
 
-Bonus exercises:
-### Demo page 141:###
--Download the code to the book. Wipe the code from the js-file and rewrite it with the help from the book.
--For additional reading and training go to: http://www.w3schools.com/jsref/jsref_obj_string.asp
- and look at JS String, JS Number, JS Math and JS Date
+var alertMessage = 'Different window properties ';
+alertMessage += ' Inner width: ' + window.innerWidth;
+alertMessage += ' Inner height: ' + window.innerHeight;
+alertMessage += ' Which URL am I at? ' + window.location;
+function myMethod() {
+    alert(alertMessage);
+}
 
-*/
+
+
+//4.Add a stylesheet that displays the changed words in fat font and in red.
+//5. PLay with the Methods page 126
+//### DOM page 126:###
+//6. Play with the DOM-properties
+
+//### String objects page 128, 129: ###
+
+//7. Save the the text from the makeMeAnArray-paragraph into an array.
+//8. Use all the string methods and propertys allong with the array
+
+//### String objects page 132: ###
+//9. check if the 4th element in the array is a number
+
+//### Math page 134: ###
+//10. Round one of the numbers in the paragraph up/down
+//11. replace the 3rd word with PI then roud it to the nearest integer
+
+//### Date object### page 137
+//12. Calculate how many days it's until your birthday and present it.
+//13. Calculate how many minutes old you are and present it.
+
+//Bonus exercises:
+//### Demo page 141:###
+//-Download the code to the book. Wipe the code from the js-file and rewrite it with the help from the book.
+//-For additional reading and training go to: http://www.w3schools.com/jsref/jsref_obj_string.asp
+// and look at JS String, JS Number, JS Math and JS Date
+
+
