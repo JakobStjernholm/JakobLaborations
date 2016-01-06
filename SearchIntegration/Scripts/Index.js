6191245a77693aa01c20f7f8d1e1e8300e7dd12b
Jakob Stjernholm <jakob.stjerna@gmail.com>
2016-01-06 16:59:37 +0100

    Search function works with phones.json file. Some CSS and then I will be adding the other json files

--- SearchIntegration/Scripts/Index.js
+++ SearchIntegration/Scripts/Index.js
@@ -1,15 +1,26 @@
 ﻿$(function () {
     $('#search').keyup(function () {
         var mySearchField = $('#search').val();
-        var regExpression = new RegExp(mySearchField, 'i');
-        $.getJSON('phones.json', function (data) {
-            var dataOutput = "<ul> class='search-results'>"
+        var regExpression = new RegExp(mySearchField, "i");
+        $.getJSON("\JSON/phones.json", function (data) {
+            var dataOutput = "<ul class='search-results'>";
 
-            $.each(data, function (key, value) {
+            $.each(data, function (key, val) {
                 if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
-
-                }
+                    dataOutput += "<li>";
+                    dataOutput += "<p>";
+                    dataOutput += "<strong>" + val.name + "</strong>";
+                    dataOutput += "</p>";
+                    dataOutput += "<br><img src='\Images/" + val.url_image + "'><br>";
+                    dataOutput += "</p>";
+                    dataOutput += val.about;
+                    dataOutput += "<p>";
+                    dataOutput += "</p>";
+                    dataOutput += "</li>";
+                };
             });
+            dataOutput += "</ul>";
+            $('#presented-data').html(dataOutput);
         });
     });
 });
