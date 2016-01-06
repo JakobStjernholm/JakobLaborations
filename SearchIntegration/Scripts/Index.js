7864941475b647f69a5b02197e9f73d5077e6191
Jakob Stjernholm <jakob.stjerna@gmail.com>
2016-01-06 17:46:04 +0100

    Added checkboxes, if checkbox is checked it will get the json.

--- SearchIntegration/Scripts/Index.js
+++ SearchIntegration/Scripts/Index.js
@@ -4,22 +4,26 @@
         var regExpression = new RegExp(mySearchField, "i");
         $.getJSON("\JSON/phones.json", function (data) {
             var dataOutput = "<ul class='search-results'>";
+            var phoneCheckbox = $('#phone').is(':checked');
+            if (phoneCheckbox === true) {
 
-            $.each(data, function (key, val) {
-                if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
-                    dataOutput += "<li>";
-                    dataOutput += "<p>";
-                    dataOutput += "<strong>" + val.name + "</strong>";
-                    dataOutput += "</p>";
-                    dataOutput += "<br><img src='\Images/" + val.url_image + "'><br>";
-                    dataOutput += "</p>";
-                    dataOutput += val.about;
-                    dataOutput += "<p>";
-                    dataOutput += "</p>";
-                    dataOutput += "</li>";
-                };
-            });
-            dataOutput += "</ul>";
+
+                $.each(data, function(key, val) {
+                    if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
+                        dataOutput += "<li>";
+                        dataOutput += "<p>";
+                        dataOutput += "<strong>" + val.name + "</strong>";
+                        dataOutput += "</p>";
+                        dataOutput += "<br><img src='\Images/" + val.url_image + "'><br>";
+                        dataOutput += "</p>";
+                        dataOutput += val.about;
+                        dataOutput += "<p>";
+                        dataOutput += "</p>";
+                        dataOutput += "</li>";
+                    };
+                });
+                dataOutput += "</ul>";
+            }
             $('#presented-data').html(dataOutput);
         });
     });
