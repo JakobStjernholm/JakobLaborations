8a602b69c6ba202547f829db89686b4b5e92dabe
Jakob Stjernholm <jakob.stjerna@gmail.com>
2016-01-06 16:16:35 +0100

    Making jQuery in the JavaScript file. Collecting phones.json file,

--- SearchIntegration/Scripts/Index.js
+++ SearchIntegration/Scripts/Index.js
@@ -1,1 +1,15 @@
-﻿
+﻿$(function () {
+    $('#search').keyup(function () {
+        var mySearchField = $('#search').val();
+        var regExpression = new RegExp(mySearchField, 'i');
+        $.getJSON('phones.json', function (data) {
+            var dataOutput = "<ul> class='search-results'>"
+
+            $.each(data, function (key, value) {
+                if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
+
+                }
+            });
+        });
+    });
+});
