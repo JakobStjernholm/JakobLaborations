ea04ae457db63407a6c7d2e635cd291ae577ac55
Jakob Stjernholm <jakob.stjerna@gmail.com>
2016-01-07 12:24:43 +0100

    Done with Laboration03, refactored to functions..

--- SearchIntegration/Scripts/Index.js
+++ SearchIntegration/Scripts/Index.js
@@ -1,95 +1,102 @@
 ﻿$(function () {
     $('#search').focus();
+    showResult();
     $('#search').keyup(function () {
-        var mySearchField = $('#search').val();
-        var regExpression = new RegExp(mySearchField, "i");
-        var phoneCheckbox = $('#phone').is(':checked');
-        var tvCheckbox = $('#tv').is(':checked');
-        var tabletCheckbox = $('#tablets').is(':checked');
-        var laptopCheckbox = $('#laptop').is(':checked');
-        var dataOutput = "<ul class='search-results'>";
-        if (phoneCheckbox === true) {
-            $.getJSON("\JSON/phones.json", function (data) {
-
-
-                $.each(data, function (key, val) {
-                    if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
-                        dataOutput += "<li>";
-                        dataOutput += "<p>";
-                        dataOutput += "<strong>" + val.name + "</strong>";
-                        dataOutput += "</p>";
-                        dataOutput += "<br><img src='\Images/" + val.url_image + "'><br>";
-                        dataOutput += "</p>";
-                        dataOutput += val.about;
-                        dataOutput += "<p>";
-                        dataOutput += "</p>";
-                        dataOutput += "</li>";
-                    };
-                });
-
-            });
-        }
-        if (tvCheckbox === true) {
-            $.getJSON("\JSON/tv.json", function (data) {
-
-                $.each(data, function (key, val) {
-                    if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
-                        dataOutput += "<li>";
-                        dataOutput += "<p>";
-                        dataOutput += "<strong>" + val.name + "</strong>";
-                        dataOutput += "</p>";
-                        dataOutput += "<br><img src='\Images/" + val.url_image + "'><br>";
-                        dataOutput += "</p>";
-                        dataOutput += val.about;
-                        dataOutput += "<p>";
-                        dataOutput += "</p>";
-                        dataOutput += "</li>";
-                    };
-                });
-                $('#presented-data').html(dataOutput);
-            });
-        }
-        if (tabletCheckbox === true) {
-            $.getJSON("\JSON/tablets.json", function (data) {
-
-                $.each(data, function (key, val) {
-                    if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
-                        dataOutput += "<li>";
-                        dataOutput += "<p>";
-                        dataOutput += "<strong>" + val.name + "</strong>";
-                        dataOutput += "</p>";
-                        dataOutput += "<br><img src='\Images/" + val.url_image + "'><br>";
-                        dataOutput += "</p>";
-                        dataOutput += val.about;
-                        dataOutput += "<p>";
-                        dataOutput += "</p>";
-                        dataOutput += "</li>";
-                    };
-                });
-                $('#presented-data').html(dataOutput);
-            });
-        }
-        if (laptopCheckbox === true) {
-            $.getJSON("\JSON/laptops.json", function (data) {
-
-                $.each(data, function (key, val) {
-                    if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
-                        dataOutput += "<li>";
-                        dataOutput += "<p>";
-                        dataOutput += "<strong>" + val.name + "</strong>";
-                        dataOutput += "</p>";
-                        dataOutput += "<br><img src='\Images/" + val.url_image + "'><br>";
-                        dataOutput += "</p>";
-                        dataOutput += val.about;
-                        dataOutput += "<p>";
-                        dataOutput += "</p>";
-                        dataOutput += "</li>";
-                    };
-                });
-                $('#presented-data').html(dataOutput);
-            });
-        }
-        dataOutput += "</ul>";
-        $('#presented-data').html(dataOutput);
+        showResult();
     });
 });
+function showResult() {
+    var mySearchField = $('#search').val();
+    var regExpression = new RegExp(mySearchField, "i");
+
+    var tvCheckbox = $('#tv').is(':checked');
+    var phoneCheckbox = $('#phone').is(':checked');
+    var tabletCheckbox = $('#tablets').is(':checked');
+    var laptopCheckbox = $('#laptop').is(':checked');
+
+    var dataOutput = "<ul class='search-results'>";
+    if (tvCheckbox === true) {
+        $.getJSON("\JSON/tv.json", function (data) {
+
+            $.each(data, function (key, val) {
+                if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
+                    dataOutput += "<li>";
+                    dataOutput += "<p>";
+                    dataOutput += "<strong>" + val.name + "</strong>";
+                    dataOutput += "</p>";
+                    dataOutput += "<br><img src='\Images/" + val.url_image + "'><br>";
+                    dataOutput += "</p>";
+                    dataOutput += val.about;
+                    dataOutput += "<p>";
+                    dataOutput += "</p>";
+                    dataOutput += "</li>";
+                };
+            });
+            $('#presented-data').html(dataOutput);
+        });
+    }
+    if (phoneCheckbox === true) {
+        $.getJSON("\JSON/phones.json", function (data) {
+
+
+            $.each(data, function (key, val) {
+                if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
+                    dataOutput += "<li>";
+                    dataOutput += "<p>";
+                    dataOutput += "<strong>" + val.name + "</strong>";
+                    dataOutput += "</p>";
+                    dataOutput += "<br><img src='\Images/" + val.url_image + "'><br>";
+                    dataOutput += "</p>";
+                    dataOutput += val.about;
+                    dataOutput += "<p>";
+                    dataOutput += "</p>";
+                    dataOutput += "</li>";
+                };
+                $('#presented-data').html(dataOutput);
+            });
+
+        });
+    }
+    if (tabletCheckbox === true) {
+        $.getJSON("\JSON/tablets.json", function (data) {
+
+            $.each(data, function (key, val) {
+                if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
+                    dataOutput += "<li>";
+                    dataOutput += "<p>";
+                    dataOutput += "<strong>" + val.name + "</strong>";
+                    dataOutput += "</p>";
+                    dataOutput += "<br><img src='\Images/" + val.url_image + "'><br>";
+                    dataOutput += "</p>";
+                    dataOutput += val.about;
+                    dataOutput += "<p>";
+                    dataOutput += "</p>";
+                    dataOutput += "</li>";
+                };
+            });
+            $('#presented-data').html(dataOutput);
+        });
+    }
+    if (laptopCheckbox === true) {
+        $.getJSON("\JSON/laptops.json", function (data) {
+
+            $.each(data, function (key, val) {
+                if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
+                    dataOutput += "<li>";
+                    dataOutput += "<p>";
+                    dataOutput += "<strong>" + val.name + "</strong>";
+                    dataOutput += "</p>";
+                    dataOutput += "<br><img src='\Images/" + val.url_image + "'><br>";
+                    dataOutput += "</p>";
+                    dataOutput += val.about;
+                    dataOutput += "<p>";
+                    dataOutput += "</p>";
+                    dataOutput += "</li>";
+                };
+            });
+            $('#presented-data').html(dataOutput);
+        });
+    }
+    dataOutput += "</ul>";
+    $('#presented-data').html(dataOutput);
+};
