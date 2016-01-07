f81a579782f9fb83278e82d9f700218e590f82ad
Jakob Stjernholm <jakob.stjerna@gmail.com>
2016-01-07 11:58:46 +0100

    Added all data. Think labb is done

--- SearchIntegration/Scripts/Index.js
+++ SearchIntegration/Scripts/Index.js
@@ -1,6 +1,5 @@
 ﻿$(function () {
     $('#search').focus();
-    $('#search').reload();
     $('#search').keyup(function () {
         var mySearchField = $('#search').val();
         var regExpression = new RegExp(mySearchField, "i");
@@ -32,8 +31,6 @@
         }
         if (tvCheckbox === true) {
             $.getJSON("\JSON/tv.json", function (data) {
-                
-
 
                 $.each(data, function (key, val) {
                     if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
@@ -49,15 +46,11 @@
                         dataOutput += "</li>";
                     };
                 });
-                dataOutput += "</ul>";
-
                 $('#presented-data').html(dataOutput);
             });
         }
         if (tabletCheckbox === true) {
             $.getJSON("\JSON/tablets.json", function (data) {
-                
-
 
                 $.each(data, function (key, val) {
                     if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
@@ -73,15 +66,11 @@
                         dataOutput += "</li>";
                     };
                 });
-                dataOutput += "</ul>";
-
                 $('#presented-data').html(dataOutput);
             });
         }
         if (laptopCheckbox === true) {
             $.getJSON("\JSON/laptops.json", function (data) {
-                
-
 
                 $.each(data, function (key, val) {
                     if ((val.name.search(regExpression) !== -1) || (val.about.search(regExpression)) !== -1) {
@@ -97,8 +86,6 @@
                         dataOutput += "</li>";
                     };
                 });
-                
-
                 $('#presented-data').html(dataOutput);
             });
         }
